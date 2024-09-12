a552edcdcc0fcdd197df7d1a8c25baa094941993
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-09-12 07:58:02 +0530

    shell-scripting/02-converstion.sh

---
+++ shell-scripting/02-converstion.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Ramesh:: Hi Shiva, how are you"
+echo "Shiva:: hi Ramesh, i am good, How about you doing?"
+echo "Ramesh:: I am doing good Shiva. What's goin on?"
+echo "Shvia:: I started learning Shell script Ramesh" 
