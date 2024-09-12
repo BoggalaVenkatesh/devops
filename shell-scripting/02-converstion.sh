8df3c107ef5411cf0eb58ecfa14a41dd2e78f351
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-09-12 10:12:33 +0530

    Terraform/

--- shell-scripting/02-converstion.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-echo "Ramesh:: Hi Shiva, how are you"
-echo "Shiva:: hi Ramesh, i am good, How about you doing?"
-echo "Ramesh:: I am doing good Shiva. What's goin on?"
-echo "Shvia:: I started learning Shell script Ramesh" 
