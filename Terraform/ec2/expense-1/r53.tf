b6e3a6681512d5f986731f84014d9dbb22d97dc5
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-09 16:13:24 +0530

    expense-1

--- Terraform/ec2/expense-1/r53.tf
+++ Terraform/ec2/expense-1/r53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
   #backend.venkatesulu.online
-  name    = var.instance_names[count.index] == "frontend" ? var.domine_name : "${var.instance_names[count.index]}.${var.domine_name}" #Interpolation ${ ... } 
+  name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}" #Interpolation ${ ... } 
   type    = "A"
   ttl     = 1
   records = var.instance_names[count.index] == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terraform[count.index].private_ip]
