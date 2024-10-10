2578160b2731af2b476a6aefa64dd6feabfe06ad
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-10 07:09:49 +0530

    locals

--- Terraform/ec2/locals/r53.tf
+++ Terraform/ec2/locals/r53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "expense" {
   count   = length(var.instance_names)
-  zone_id = var.zone_id
+  zone_id = local.zone_id
   #backend.venkatesulu.online
-  name            = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}" #Interpolation ${ ... } 
+  name            = var.instance_names[count.index] == "frontend" ? var.domain_name : "${local.instance_names[count.index]}.${local.domain_name}" #Interpolation ${ ... } 
   type            = "A"
   ttl             = 1
   records         = var.instance_names[count.index] == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terraform[count.index].private_ip]
