2e27957faa7e7339027539f8b0c85e99e02cdcaa
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-10 07:02:42 +0530

    locals

--- Terraform/ec2/locals/variables.tf
+++ Terraform/ec2/locals/variables.tf
@@ -5,9 +5,9 @@ variable "instance_names" {
 }
 
 
-variable "domain_name" {
-  default = "venkatesulu.online"
-}
+# variable "domain_name" {
+#   default = "venkatesulu.online"
+# }
 
 variable "zone_id" {
   default = "Z07797711LKH2UGDN2UVT"
