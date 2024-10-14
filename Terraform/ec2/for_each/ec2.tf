e131ba3a48613a694b275cb32c7fbf3f8fbde48b
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-14 23:54:02 +0530

    for each

--- Terraform/ec2/for_each/ec2.tf
+++ Terraform/ec2/for_each/ec2.tf
@@ -1,11 +1,12 @@
-resource "aws_instance" "terraform" {
-  count                  = length(var.instance_names)
-  ami                    = data.aws_ami.ami_info.id
-  instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
+resource "aws_instance" "expense" {
+    for_each = var.instances # this variable is map
+    # for_each will give us a special variable with name each
+    ami = "ami-09c813fb71547fc4f" # this AMI ID may change over the time
+    instance_type = each.value
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = each.key
+    }
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
