cfb1ae6c916339d8c9201bcf8f9b81070b90c8b8
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-15 00:09:39 +0530

    for each

--- Terraform/ec2/for_each/provider.tf
+++ Terraform/ec2/for_each/provider.tf
@@ -5,9 +5,15 @@ terraform {
       version = "5.66.0"
     }
   }
+
+
+  backend "s3" {
+    bucket = "81s-remote-state"
+    key    = "for-each-demo"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
 }
-
-
 
 provider "aws" {
   # Configuration options
