f1480a756bf9e6fd752be6f3d7d537b4b14d16bd
Boggala Venkatatesulu <venkateshlue11@gmail.com>
2024-10-14 23:51:56 +0530

    for each

---
+++ Terraform/ec2/for_each/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_names" {
+  type        = list(string)
+  default     = ["mysql", "backend", "frontend"]
+  description = "description"
+}
+
+
+variable "domain_name" {
+  default = "venkatesulu.online"
+}
+
+variable "zone_id" {
+  default = "Z07797711LKH2UGDN2UVT"
+}
